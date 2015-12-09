d9c25a821cccbc175a971889733c460f50a04f43
shantanupanda <shantanu.panda@talentica.com>
2015-12-09 15:07:14 +0530

    Adding all the dependencies

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,12 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 # This version supports only apache >= 2.4
+supports 'ubuntu', '>= 13.10'
 
-depends 'apache2', '~> 3.1.0'
-depends 'mysql', '~> 6.1.2'
+depends 'mysql_role', '~> 0.1'
+depends 'apache2', '>= 2.0.0'
+depends 'php'
+depends 'yum'
+depends 'apt'
 # Optional dependencies of php_application definition
+depends 'ssl'
