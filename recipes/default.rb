98c94bf74e80d67fd07978a1be99d93708a9ba4c
shantanupanda <shantanu.panda@talentica.com>
2015-12-09 11:57:55 +0530

    Added Mysql

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,5 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#Installing MySQL: This is coming from other cookbook
+include_recipe 'mysql_role::default'
+
+#Installing Apache2
 include_recipe 'lamp_chef::apache2'
+
 
