166d2052a55b1eae64eb3949a4891c5f087ee31c
shantanupanda <shantanu.panda@talentica.com>
2015-12-09 17:27:05 +0530

    Merged to Development

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
-=======
 #
 # Cookbook Name:: lamp_chef
 # Attributes:: default
 #
 
->>>>>>> development
+
 # Prefork Attributes, tune them according node memory
 default[:apache][:prefork][:startservers]        = 8
 default[:apache][:prefork][:minspareservers]     = 8
@@ -13,13 +11,9 @@ default[:apache][:prefork][:maxspareservers]     = 16
 default[:apache][:prefork][:serverlimit]         = 200
 default[:apache][:prefork][:maxclients]          = 200
 default[:apache][:prefork][:maxrequestsperchild] = 10_000
-<<<<<<< HEAD
-=======
 
 #Mysql attributes
 default[:lamp_chef][:mysql][:servicename] = 'talentica'
 default[:lamp_chef][:mysql][:port] = '3306'
 default[:lamp_chef][:mysql][:version] = '5.5'
 
-
->>>>>>> development
