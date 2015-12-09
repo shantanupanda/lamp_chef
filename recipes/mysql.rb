9f2ca15ef93ef052ea2653adfade6820d0a3eda8
shantanupanda <shantanu.panda@talentica.com>
2015-12-09 16:15:36 +0530

    Included Data Bag

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,6 +1,13 @@
-mysql_service 'talentica' do
-	port '3306'
-	version '5.5'
-	initial_root_password 'talentica'
+#
+# Cookbook Name:: lamp_chef
+# Recipe:: mysql
+#
+
+mysqlpass = data_bag_item("mysql", "root_password")
+
+mysql_service node[:lamp_chef][:mysql][:servicename] do
+	port node[:lamp_chef][:mysql][:port]
+	version node[:lamp_chef][:mysql][:version]
+	initial_root_password mysqlpass["value"]
 	action [:create, :start]
 end
